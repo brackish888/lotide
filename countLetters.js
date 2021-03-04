c105bf62f781b7baaca91815b57d6cf13975f25d
Sam T <bbrackish@protonmail.com>
2021-03-04 03:46:00 +0000

    Getting the hang of objects i think

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   console.log(actual, expected);
   if (actual === expected) {
     console.log`✅✅✅ Assertion Passed: ${actual === expected}`;
@@ -11,15 +11,15 @@ const countLetters = function(sentence) {
   for (const letter of sentence) {
     if (letter !== ' ') {
       if (result[letter]) {
-        result[letter] += 1
+        result[letter] += 1;
       } else {
-        result[letter] = 1
+        result[letter] = 1;
       }
     }
   }
   return result;
 
-}
+};
 console.log(countLetters("Letters from home"));
 const result1 = countLetters("Letters from home");
 assertEqual(result1["e"], 3);
