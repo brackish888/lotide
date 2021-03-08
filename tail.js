a085d42a4d50a3877db6f8ef3112ff1c04a6cd64
Sam T <bbrackish@protonmail.com>
2021-03-08 20:30:52 +0000

    Getting more files streamlined with embedded file functions

--- tail.js
+++ tail.js
@@ -1,16 +1,9 @@
-const assertEqual = function (actual, expected) {
-  console.log(actual, expected);
-  if (actual === expected) {
-    console.log`✅✅✅ Assertion Passed: ${actual === expected}`;
-  } else if (actual !== expected) {
-    console.log`🛑🛑🛑 Assertion Failed: ${actual !== expected}`;
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const tail = arr => {
   return arr.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs"); 
+
+
+module.exports = tail;
